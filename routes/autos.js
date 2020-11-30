41a61ae6b1e4e500f97ef31a3fe06083db9a7c2e
Leo Terry <lucasenriquez1997@gmail.com>
2020-11-30 15:42:30 -0300

    Terminado más o menos

--- routes/autos.js
+++ routes/autos.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const controlador = require("../controllers/autosController")
 
 router.get("/", controlador.autos)
-/* router.get("/:marca", controlador.marca) */
+router.get("/:marca", controlador.marca)
+router.get("/:marca/:dato",controlador.dato)
 
 module.exports = router
